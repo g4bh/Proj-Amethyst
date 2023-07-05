c3a5f9c9f26969b2939b10139d20a101129c0064
Diego Sampaio <diegosampaio505@gmail.com>
2023-07-05 15:03:10 -0300

    Correção class Usuario

--- Rede-Social/src/main/java/com/example/Rede/Social/Usuario.java
+++ Rede-Social/src/main/java/com/example/Rede/Social/Usuario.java
@@ -3,6 +3,7 @@ package com.example.Rede.Social;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.List;
+import java.util.ArrayList;
 
 @SpringBootApplication
 
@@ -15,34 +16,34 @@ public class Usuario {
 	private List<Usuario> amigos;
 	private List<Grupo> grupos;
 	private List<Usuario> seguidores;
+	private List<Pagina> paginas;
 	
-	public Usuario (int id, String nome, String foto, String localizacao, List<String> interesses, List<Usuario> amigos, List<Usuario> seguidores ) {
+	public Usuario (int id, String nome, String foto, String localizacao, List<String> interesses, List<Usuario> amigos, List<Usuario> seguidores, List<Pagina> paginas) {
 		this.id = id;
 		this.foto = foto;
 		this.localizacao = localizacao;
 		this.interesses = interesses;
-		this.amigos = amigos;
-		this.grupos = grupos;
-		this.seguidores = seguidores;
+		this.amigos = new ArrayList<>();
+        this.grupos = new ArrayList<>();
+        this.seguidores = new ArrayList<>();
+        this.paginas = new ArrayList<>();
 	}
 	
 	public void adicionarAmigo(Usuario amigo) {
-		if (this == amigo || amigos.contains(amigo)) {
-            System.out.println("Essa pessoa ja é sua amiga");
-            return;
+        if (!amigos.contains(amigo)) {
+            amigos.add(amigo);
+            amigo.adicionarAmigo(this);
         }
-		
-		amigos.add(amigo);
-		System.out.println("Amigo adicionado");
-	}
-	
-	
-	public void removerAmigo(Usuario amigo) {
-		
-		if (amigos.contains(amigo)) {
+    }
+
+    public void removerAmigo(Usuario amigo) {
+        if (amigos.contains(amigo)) {
             amigos.remove(amigo);
-            System.out.println("amigos excluido");
+            amigo.removerAmigo(this);
         }
-		
-}}
+    }
+	
+	
+
+}
 
